e419127012b1017c400aa873ebcfec1d526e01b8
casandraescobedo <126704586+casandraescobedo@users.noreply.github.com>
2025-01-27 21:10:40 +0000

    Completed the lab

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,11 @@
 // Hello World
+public class Hello{
+
+    public static void main(String[]args){
+        //output hello world
+    
+        // cout << "Hello World" << endl;
+        System.out.println("Hello World");
+
+    } // no need for a semicolon 
+} // no need for a semicolon 
